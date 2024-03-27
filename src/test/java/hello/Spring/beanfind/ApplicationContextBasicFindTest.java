0f221033be05f6eab41c002146c2a874a6e76856
ddangme <ssilver121@naver.com>
2024-03-28 01:43:35 +0900

    test: 스프링 빈 조회 - 기본 코드

---
+++ src/test/java/hello/Spring/beanfind/ApplicationContextBasicFindTest.java
@@ -0,0 +1,46 @@
+package hello.Spring.beanfind;
+
+import hello.Spring.AppConfig;
+import hello.Spring.member.MemberService;
+import hello.Spring.member.MemberServiceImpl;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ApplicationContextBasicFindTest {
+
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @DisplayName("빈 이름으로 조회")
+    @Test
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @DisplayName("이름 없이 타입으로만 조회")
+    @Test
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    // 구체 타입으로 조회하면 변경시 유연성이 떨어진다.
+    @Test
+    @DisplayName("구체 타입으로 조회") void findBeanByName2() {
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X") void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class, () -> ac.getBean("xxxxx", MemberService.class));
+    }
+
+
+}
--- src/test/java/hello/Spring/beanfind/ApplicationContextInfoTest.java
+++ src/test/java/hello/Spring/beanfind/ApplicationContextInfoTest.java
@@ -10,26 +10,28 @@ public class ApplicationContextInfoTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
-    @DisplayName("")
+    @DisplayName("모든 빈 출력하기")
     @Test
     void findAllBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("beanDefinitionName = " + beanDefinitionName + "object = " + bean);
+            System.out.println("beanDefinitionName = " + beanDefinitionName + ",object = " + bean);
         }
     }
 
-    @DisplayName("")
+    @DisplayName("애플리케이션 빈 출력하기")
     @Test
     void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
+            // ROLE_APPLICATION: 직접 등록한 애플리케이션 빈
+            // ROLE_INFRASTRUCTURE: 스프링 내부에서 사용하는 빈
             if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
                 Object bean = ac.getBean(beanDefinitionName);
-                System.out.println("beanDefinitionName = " + beanDefinitionName + "object = " + bean);
+                System.out.println("beanDefinitionName = " + beanDefinitionName + ",object = " + bean);
             }
         }
     }
