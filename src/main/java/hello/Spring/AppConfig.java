8af7dd95abc294d63b2a90a14bde35abf18914fb
ddangme <ssilver121@naver.com>
2024-03-27 23:44:09 +0900

    refactor: AppConfig 리팩토링

--- src/main/java/hello/Spring/AppConfig.java
+++ src/main/java/hello/Spring/AppConfig.java
@@ -1,5 +1,6 @@
 package hello.Spring;
 
+import hello.Spring.discount.DiscountPolicy;
 import hello.Spring.discount.FixDiscountPolicy;
 import hello.Spring.member.MemberService;
 import hello.Spring.member.MemberServiceImpl;
@@ -10,15 +11,20 @@ import hello.Spring.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    private static MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 
-
+    private DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 
 
 }
