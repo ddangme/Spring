d53eb9aa576387452131e125d6fbb0187adf034e
ddangme <ssilver121@naver.com>
2024-03-27 23:36:44 +0900

    test: AppConfig 적용하도록 수정

--- src/test/java/hello/Spring/member/MemberServiceTest.java
+++ src/test/java/hello/Spring/member/MemberServiceTest.java
@@ -1,12 +1,20 @@
 package hello.Spring.member;
 
+import hello.Spring.AppConfig;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
--- src/test/java/hello/Spring/order/OrderServiceTest.java
+++ src/test/java/hello/Spring/order/OrderServiceTest.java
@@ -1,17 +1,26 @@
 package hello.Spring.order;
 
+import hello.Spring.AppConfig;
 import hello.Spring.member.Grade;
 import hello.Spring.member.Member;
 import hello.Spring.member.MemberService;
 import hello.Spring.member.MemberServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class OrderServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
