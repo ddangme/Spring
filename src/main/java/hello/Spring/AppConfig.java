217d842f557a9ac636b08130fbd63fd9752c2c39
ddangme <ssilver121@naver.com>
2024-03-28 00:07:59 +0900

    feat: 할인 정책 수정

--- src/main/java/hello/Spring/AppConfig.java
+++ src/main/java/hello/Spring/AppConfig.java
@@ -2,6 +2,7 @@ package hello.Spring;
 
 import hello.Spring.discount.DiscountPolicy;
 import hello.Spring.discount.FixDiscountPolicy;
+import hello.Spring.discount.RateDiscountPolicy;
 import hello.Spring.member.MemberService;
 import hello.Spring.member.MemberServiceImpl;
 import hello.Spring.member.MemoryMemberRepository;
@@ -23,7 +24,7 @@ public class AppConfig {
     }
 
     private DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 
 
