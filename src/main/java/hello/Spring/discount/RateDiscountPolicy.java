98b667413b4b3cb562473e8cb5c95689b205a89a
ddangme <ssilver121@naver.com>
2024-03-27 22:47:07 +0900

    feat: 할인 정책 개발

---
+++ src/main/java/hello/Spring/discount/RateDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.Spring.discount;
+
+import hello.Spring.member.Grade;
+import hello.Spring.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    public static final int DISCOUNT_PERCENT = 10;
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * DISCOUNT_PERCENT / 100;
+        }
+
+         return 0;
+    }
+}
